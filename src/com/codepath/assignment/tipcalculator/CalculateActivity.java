cc83cda9f219e2c9dd5d2a177d40e8af30d3ee21
Linda Yang <lindahyang@gmail.com>
2013-10-08 22:56:42 -0700

    added edittext onchange listener

--- src/com/codepath/assignment/tipcalculator/CalculateActivity.java
+++ src/com/codepath/assignment/tipcalculator/CalculateActivity.java
@@ -5,6 +5,10 @@ import java.text.DecimalFormat;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -18,6 +22,7 @@ public class CalculateActivity extends Activity {
 	private EditText etAmount;
 	private EditText etOtherPct;
 	private TextView tvTipAmt;
+	private double tipPercent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +31,41 @@ public class CalculateActivity extends Activity {
 		etAmount = (EditText) findViewById(R.id.etAmountValue);
 		etOtherPct = (EditText) findViewById(R.id.etOtherPct);
 		tvTipAmt = (TextView) findViewById(R.id.tvTipValue);
-		//InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-	    //mgr.showSoftInput(etAmount, InputMethodManager.SHOW_IMPLICIT);
-	    //mgr.toggleSoftInput(InputMethodManager.SHOW_FORCED,1);
+		etAmount.setRawInputType(InputType.TYPE_CLASS_NUMBER);
+		tipPercent=0;
+		etAmount.addTextChangedListener(new TextWatcher(){
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				// TODO Auto-generated method stub
+				String totalAmount=etAmount.getText().toString();
+				if ( totalAmount != null && !totalAmount.isEmpty()){
+					try {
+						amount=Double.parseDouble(totalAmount);
+						DecimalFormat df = new DecimalFormat("$#.00");
+						tvTipAmt.setText(df.format(amount * tipPercent));
+					} catch (NumberFormatException e){
+						Toast.makeText(getBaseContext(), "Please enter a valid amount", Toast.LENGTH_SHORT).show();
+					}
+				} else {
+					tvTipAmt.setText("");
+				}
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 
 	@Override
@@ -39,21 +76,25 @@ public class CalculateActivity extends Activity {
 	}
 
 	public void onTen(View view){
-		this.calculate(0.1);
+		tipPercent=0.1;
+		this.calculate();
 	}
 	
 	public void onFifteen(View view){
-		this.calculate(0.15);
+		tipPercent=0.15;
+		this.calculate();
 	}
 	
 	public void onTwenty(View view){
-		this.calculate(0.2);
+		tipPercent=0.2;
+		this.calculate();
 	}
 	
 	public void onOther(View view) {
 		try {
 			Double otherPct = Double.parseDouble(etOtherPct.getText().toString());
-			this.calculate(otherPct.doubleValue()/100);
+			tipPercent = otherPct.doubleValue()/100;
+			this.calculate();
 		} catch(NumberFormatException e) {
 			Toast.makeText(this, "Please enter a valid tip percent", Toast.LENGTH_SHORT).show();
 		}
@@ -67,12 +108,12 @@ public class CalculateActivity extends Activity {
 	    }
 	}
 	
-	private void calculate(double percent){
+	private void calculate(){
 		try {
 			Double amountValue = Double.parseDouble(etAmount.getText().toString());
 			DecimalFormat df = new DecimalFormat("$#.00");
 			amount = amountValue.doubleValue();
-			double tip = amount * percent;
+			double tip = amount * tipPercent;
 			tvTipAmt.setText(df.format(tip));
 		} catch (NumberFormatException e){
 			Toast.makeText(this, "Please enter a valid amount", Toast.LENGTH_SHORT).show();
